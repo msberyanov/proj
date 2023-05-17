e577ddfaf6a4a81956905364b0603f4d3bc2fd09
Maxim Beryanov <MSBeryanov>
2023-05-17 18:08:51 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
+import "./App.css";
 
 const apiGateway = "https://d5d5n7jhqpq0ije398ch.apigw.yandexcloud.net";
 
+interface Session {
+  id: string;
+  name: string;
+}
+
 export const App: React.FC = () => {
   const [message, setMessage] = useState("");
+  const [sessions, setSessions] = useState<Session[]>([]);
 
-  const onClick = (path: string) => {
+  const authActions = (path: string) => {
     axios
       .get(`${apiGateway}/${path}`,
         {
           withCredentials: true
         }
       )
-      .then(response => setMessage(response.data));
+      .then(response => {
+        setMessage(response.data);
+        // sessionsActions();
+      });
   };
 
-  return (
-    <div>
-      <button onClick={() => onClick('login')}>
-        login
-      </button>
-      <button onClick={() => onClick('logout')}>
-        logout
-      </button>
-      <button onClick={() => onClick('sessions')}>
-        sessions
-      </button>
+  const sessionsActions = () => {
+    axios
+      .get(`${apiGateway}/sessions`,
+        {
+          withCredentials: true
+        }
+      )
+      .then(response => setSessions(response.data));
+  };
 
-      <div>
-        {message}
+  // useEffect(() => {
+  //   sessionsActions();
+  //
+  //   setTimeout(() => {
+  //     sessionsActions();
+  //   }, 5000);
+  // }, []);
+
+  return (
+    <div className="div-1">
+      <div className="div-2">
+        <div className="title">
+          Actions
+        </div>
+        <div className="buttons">
+          <button
+            className="button"
+            onClick={() => authActions('login')}
+          >
+            Login
+          </button>
+          <button
+            className="button"
+            onClick={() => authActions('logout')}
+          >
+            Logout
+          </button>
+        </div>
+
+        {message && (
+          <>
+            <div className="title">
+              Message
+            </div>
+            <div>
+              {message}
+            </div>
+          </>
+        )}
+      </div>
+
+      <div className="div-2">
+        <div className="title">
+          Sessions
+        </div>
+        <div>
+          <ul>
+            {sessions.map(session =>
+              <li>
+                {session.name}
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
